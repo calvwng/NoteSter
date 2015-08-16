2346eff43523b0156ea0bbabaf201e2b94decebd
Eastae <keastma4@my.smccd.edu>
2015-08-16 15:14:19 -0700

    Changed thumbs
    Changed thumbs to buttons and changed css for votecount and buttons

--- app/views/main.js
+++ app/views/main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 		$.each( data, function( key, val ) {
 			$('#list').append(
 				'<li>\
-							<div class="panel">\
+							<div id = "card'+val.id+'" class="panel">\
 													<div class="panel-heading">\
 																<div id="headerText" class="pull-left">\
 																			<h3 class="title">' + val.title + '</h3>\
@@ -11,35 +11,23 @@ $(document).ready(function() {
 																</div>\
 																<div class="upDownVote pull-right">\
 																						<div id = "voteCount" class = "tally ">' + val.votes + '</div>\
-																						<a id = "upVoteButton" href = "#">' +
+																						<button id = "upVoteButton">' +
 																									'<span class = "glyphicon glyphicon-thumbs-up thumb upDownVote" aria-hidden="true"> </span>\
-																						</a>\
-																						<a id = "downVoteButton" href = "#">\
-																									<span class = "glyphicon glyphicon-thumbs-down thumb upDownVote" aria-hidden="true"></span>\
-																						</a>\
+																						</button>\
+																						<button id = upButton>\
+																						<span class = "glyphicon glyphicon-thumbs-down thumb upDownVote" aria-hidden="true"></span>\
+																						</button>\
 																</div>\
 													</div>\
 													<div class="panel-body">\
-																<p class = "col-sm-10 desc pull-left">'+ val.description.slice(0,255) +'...<a href="#" class = "">Read More</a></p>\
+																<p class = "col-sm-10 desc pull-left">'+ val.description.slice(0,255) +'...<a href="/cards/#{'+val.id+'}" class = "">Read More</a></p>\
 													</div>\
 							</div>\
 				</li>'
 			);
-			// $('#list').append(
-			// '<li>' +
-			//     '<h2><a id = "resourceTitle" href = # class = "title pull-left col-lg-12">' + val.title +'</a></h2><br>' +
-			//     '<h3><a href="#" class = "authorName pull-left col-lg-12">'+val.author+'</a></h3>'+
-			// 				'<div class = "upDownVote col-md-2 pull-right">' +
-			//           '<div id = "voteCount" class = "tally ">' + val.votes + '</div>' +
-			//           '<a id = "upVoteButton" href = "#">' +
-			//              '<span class = "glyphicon glyphicon-thumbs-up thumb upDownVote" aria-hidden="true"> </span>'+
-			// 		        '</a>' +
-			//           '<a id = "downVoteButton" href = "#">' +
-			//              '<span class = "glyphicon glyphicon-thumbs-down thumb upDownVote" aria-hidden="true"></span>' +
-			//           '</a>' +
-			//     '</div>' +
-			//     '<p class = "col-sm-10 desc pull-left">'+val.description.slice(0,255)+'...<a href="#" class = "">Read More</a></p>' +
-			// '</li>');
+			//begin event handler
+			$("#card" + val.id).find("voteCount")
 		});
 	});
+	
 });
