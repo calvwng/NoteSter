2823200f3679286d6be12108b7b4c388e13a5894
Andrew Ryan Carpenter <andrewryancarpenter77@gmail.com>
2015-08-16 17:57:47 -0700

    one small tiny fix

--- app/controllers/resources.rb
+++ app/controllers/resources.rb
@@ -24,12 +24,29 @@ end
 get '/search' do
   erb :"/search"
 end
+
+get '/query' do
+  tags = ["ex","cumque"]
+  tags_inside_db = []
+  tags.each do |tag|
+    tags_inside_db.push( Tag.find_by(name: tag) ) if (Tag.find_by(name: tag))
+  end
+  search_results = []
+  tags_inside_db.each do |tag|
+    search_results.push << tag.resources
+    #p [tag,tag.resources]
+  end
+  return {
+      array_of_resources: search_results
+    }.to_json
+end
+
 post '/query' do
   tags = params[:tags]
   p '*'*100
   p params
   p '*'*100
-  #tags = ["ex","cumque"]
+  tags = ["ex","cumque"]
   tags_inside_db = []
   tags.each do |tag|
     tags_inside_db.push( Tag.find_by(name: tag) ) if (Tag.find_by(name: tag))
